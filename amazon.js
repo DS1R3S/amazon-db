e6b44a0d14395eb7b5c2bb8a0b68f39cfcf65b39
Dan Sires <dsires13@mac.com>
2019-02-11 11:23:22 -0500

    added screenshots

--- amazon.js
+++ amazon.js
@@ -28,33 +28,98 @@ function start() {
     connection.query(
         'SELECT * FROM products', function (err, results) {
             if (err) throw err;
-            console.log(results);
+            // console.log(results);
+            for (var i = 0; i < results.length; i++) {
+                console.log('--------------------');
+                console.log('Product ID: ' + results[i].id);
+                console.log('Product: ' + results[i].product_name);
+                console.log('Price: ' + results[i].price);
+                console.log('Quantity ' + results[i].inventory);
+            }
+            purchase();
 
         });
-    inquirer
-        .prompt([{
-            name: 'productId',
-            type: 'input',
-            message: "What product would you like to buy?"
+    // inquirer
+    //     .prompt([{
+    //         name: 'productId',
+    //         type: 'input',
+    //         message: "What product would you like to buy?"
 
-        }, {
-            name: 'quantity',
-            type: 'input',
-            message: "How many would like to buy?"
-        },
-        
-           
-    
-    
-    ]).then(function(answer){
-        console.log(answer);
-        connection.query(
-            'SELECT * FROM products WHERE id=' + answer.productId, function (err, results) {
-                if (err) throw err;
-                console.log(results);
+    //     }, {
+    //         name: 'quantity',
+    //         type: 'input',
+    //         message: "How many would like to buy?"
+    //     },
+    //     ]).then(function (answer) {
+    //         console.log(answer);
+    //         connection.query(
+    //             'SELECT * FROM products WHERE id=' + answer.productId, function (err, results) {
+    //                 if (err) throw err;
+    //                 console.log(results);
+    //             }
+    //         );
+    //     });
+}
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole number.';
+    }
+}
+function purchase() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'product_id',
+        message: 'Select product id of item you would like to buy.',
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "quantity",
+        message: "How many of this item would you like to purchase?",
+        validate: validateInput,
+
+    }
+    ]).then(function (purchase) {
+        var item = purchase.product_id;
+        var quantity = purchase.quantity;
+
+        connection.query('SELECT * FROM products WHERE ?', { id: item }, function (err, results) {
+            if (err) throw err;
+            if (results.length === 0) {
+                console.log("ERROR: Please select a valid product id.");
+                start();
+            } else {
+                var productInfo = results[0];
+
+                if (quantity <= productInfo.inventory) {
+                    console.log(productInfo.product_name + 'is available.  Placing your order.');
+                    var updateQuery = "UPDATE products SET inventory = " +
+                        (productInfo.inventory - quantity) + " WHERE id = " + item;
+
+                    connection.query(updateQuery, function (err, data) {
+                        if (err) throw err;
+                        console.log("Your order has been placed!");
+                        console.log("Your total is $" + productInfo.price * quantity);
+                        console.log("Thank you for shopping with us!");
+                        console.log(" - - - - - - - - - - - - - - - ");
+                        console.log("To shop again with us please input 'node amazon.js' into your command line.");
+                        console.log("\n");
+
+                        connection.end();
+                    });
+                } else {
+                    console.log('There is not enough ' + productInfo.product_name + ' in stock');
+                    console.log("Your order can not be placed as is.");
+                    console.log("Please modify your order or select another item.");
+                    setTimeout(function() { start() }, 3000);
+                }
             }
-        );
+
+        });
     });
-    
-    
 }
